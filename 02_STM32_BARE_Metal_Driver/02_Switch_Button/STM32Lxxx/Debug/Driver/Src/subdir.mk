be77766f4dcfb40352f75ede571eb458464862c3
aniket omanna <aniket.omanna15@gmail.com>
2025-09-16 09:39:39 +0530

    Add 02_STM32_BARE_Metal_Driver/02_Switch_Button

---
+++ 02_STM32_BARE_Metal_Driver/02_Switch_Button/STM32Lxxx/Debug/Driver/Src/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (13.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Driver/Src/STM32Lxx_gpio_driver.c 
+
+OBJS += \
+./Driver/Src/STM32Lxx_gpio_driver.o 
+
+C_DEPS += \
+./Driver/Src/STM32Lxx_gpio_driver.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Driver/Src/%.o Driver/Src/%.su Driver/Src/%.cyclo: ../Driver/Src/%.c Driver/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32L4 -DSTM32 -DNUCLEO_L476RG -DSTM32L476RGTx -c -I../Inc -I"D:/BitSilica/STM_PRO/STM32Lxxx/Driver/Inc" -I"D:/BitSilica/STM_PRO/STM32Lxxx/Driver/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-Driver-2f-Src
+
+clean-Driver-2f-Src:
+	-$(RM) ./Driver/Src/STM32Lxx_gpio_driver.cyclo ./Driver/Src/STM32Lxx_gpio_driver.d ./Driver/Src/STM32Lxx_gpio_driver.o ./Driver/Src/STM32Lxx_gpio_driver.su
+
+.PHONY: clean-Driver-2f-Src
+
