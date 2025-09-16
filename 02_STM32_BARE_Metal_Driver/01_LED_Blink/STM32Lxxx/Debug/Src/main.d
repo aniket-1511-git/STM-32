975cefdf865bf7fdaae71ecef4b5ea69f990dc55
aniket omanna <aniket.omanna15@gmail.com>
2025-09-16 09:38:57 +0530

    Add 02_STM32_BARE_Metal_Driver/01_LED_Blink

---
+++ 02_STM32_BARE_Metal_Driver/01_LED_Blink/STM32Lxxx/Debug/Src/main.d
@@ -0,0 +1,8 @@
+Src/main.o: ../Src/main.c ../Inc/main.h \
+ D:/BitSilica/STM_PRO/STM32Lxxx/Driver/Inc/STM32Lxx_gpio_header.h \
+ D:/BitSilica/STM_PRO/STM32Lxxx/Driver/Inc/STM32Lxx.H \
+ D:/BitSilica/STM_PRO/STM32Lxxx/Driver/Inc/STM32Lxx.h
+../Inc/main.h:
+D:/BitSilica/STM_PRO/STM32Lxxx/Driver/Inc/STM32Lxx_gpio_header.h:
+D:/BitSilica/STM_PRO/STM32Lxxx/Driver/Inc/STM32Lxx.H:
+D:/BitSilica/STM_PRO/STM32Lxxx/Driver/Inc/STM32Lxx.h:
