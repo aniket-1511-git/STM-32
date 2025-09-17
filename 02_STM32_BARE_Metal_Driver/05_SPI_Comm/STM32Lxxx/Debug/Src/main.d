74374f7ca0c9fae26f12d5057f92063ed65c9edd
aniket omanna <aniket.omanna15@gmail.com>
2025-09-17 09:41:44 +0530

    Add 02_STM32_BARE_Metal_Driver/05_SPI_Comm

---
+++ 02_STM32_BARE_Metal_Driver/05_SPI_Comm/STM32Lxxx/Debug/Src/main.d
@@ -0,0 +1,12 @@
+Src/main.o: ../Src/main.c ../Inc/main.h \
+ D:/BitSilica/STM_PRO/STM32Lxxx/Driver/Inc/STM32Lxx_gpio_header.h \
+ D:/BitSilica/STM_PRO/STM32Lxxx/Driver/Inc/STM32Lxx.H \
+ D:/BitSilica/STM_PRO/STM32Lxxx/Driver/Inc/STM32Lxx_SPIx_header.h \
+ D:/BitSilica/STM_PRO/STM32Lxxx/Driver/Inc/STM32Lxx.h \
+ D:/BitSilica/STM_PRO/STM32Lxxx/Driver/Inc/STM32Lxx.h
+../Inc/main.h:
+D:/BitSilica/STM_PRO/STM32Lxxx/Driver/Inc/STM32Lxx_gpio_header.h:
+D:/BitSilica/STM_PRO/STM32Lxxx/Driver/Inc/STM32Lxx.H:
+D:/BitSilica/STM_PRO/STM32Lxxx/Driver/Inc/STM32Lxx_SPIx_header.h:
+D:/BitSilica/STM_PRO/STM32Lxxx/Driver/Inc/STM32Lxx.h:
+D:/BitSilica/STM_PRO/STM32Lxxx/Driver/Inc/STM32Lxx.h:
